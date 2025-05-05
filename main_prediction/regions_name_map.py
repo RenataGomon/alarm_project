be5d48acc3e3e09cca111d236810a95568827d7e
RenataGomon <165139128+RenataGomon@users.noreply.github.com>
2025-05-05 16:08:28 +0300

    renamed Zaporizhzhia name

--- main_prediction/regions_name_map.py
+++ main_prediction/regions_name_map.py
@@ -1,6 +1,6 @@
 region_map = {
     1: "Simferopol", 2: "Vinnytsia", 3: "Lutsk", 4: "Dnipro", 5: "Donetsk",
-    6: "Zhytomyr", 7: "Uzhgorod", 8: "Zaporozhye", 9: "Ivano-Frankivsk",
+    6: "Zhytomyr", 7: "Uzhgorod", 8: "Zaporizhzhia", 9: "Ivano-Frankivsk",
     10: "Kyiv_obl", 11: "Kropyvnytskyi", 12: "Luhansk", 13: "Lviv", 14: "Mykolaiv",
     15: "Odesa", 16: "Poltava", 17: "Rivne", 18: "Sumy", 19: "Ternopil",
     20: "Kharkiv", 21: "Kherson", 22: "Khmelnytskyi", 23: "Cherkasy", 24: "Chernivtsi",
